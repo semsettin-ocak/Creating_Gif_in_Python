6a6b5d160068a500dd4b6d85d81cae7e02ca8f87
Şemsettin OCAK <ocaksemsettin@gmail.com>
2023-10-09 17:19:27 +0300

    Creating GIF
    Convert images to GIF

---
+++ GifCreator/main.py
@@ -0,0 +1,7 @@
+from PIL import Image
+
+image_path_files = ['Dog_1.jpg','Dog_2.jpg','Dog_3.jpg']
+
+listed_images = [ Image.open(file) for file in image_path_files ]
+
+listed_images[0].save('animation.gif', save_all=True, append_images = listed_images[1:], duration=1000, loop=0)
