359d6340345f5fb9a2e741d5dc430e07bdcf6e2e
Şemsettin Ocak <ocaksemsettin@gmail.com>
2024-03-28 15:57:11 +0300

    print kodunu ekledim

--- GifCreator/main.py
+++ GifCreator/main.py
@@ -5,3 +5,5 @@ image_path_files = ['Dog_1.jpg','Dog_2.jpg','Dog_3.jpg']
 listed_images = [ Image.open(file) for file in image_path_files ]
 
 listed_images[0].save('animation.gif', save_all=True, append_images = listed_images[1:], duration=1000, loop=0)
+
+print("thank you")
